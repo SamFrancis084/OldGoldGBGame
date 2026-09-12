6c4eaffd14490dd1e1fc9914f92d1b87be433dd6
Sam Francis <samfrancis084@gmail.com>
2026-09-12 17:23:09 +1000

    test

--- old-gold-game/Scripts/Panner.gd
+++ old-gold-game/Scripts/Panner.gd
@@ -7,6 +7,11 @@ var start_pos : Vector2 = Vector2.ZERO
 @onready var gold_sprite : Sprite2D = $PanSprite/GoldSprite
 @onready var dirt_sprite = $PanSprite/DirtSprite
 
+@onready var label : Label = $CanvasLayer/Control/Label
+
+enum av_buttons {LEFT, RIGHT, UP, DOWN} #can add other buttons
+var desired_button : av_buttons
+
 var times_shaken : int = 0 # for testing
 var times_to_shake : int = 3
 
@@ -16,9 +21,13 @@ var move_input : Vector2 = Vector2.ZERO
 func _ready():
 	pass # Replace with function body.
 
+func get_random_button():
+	desired_button = av_buttons.keys()[randi() % av_buttons.size()]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	button_tracker()
+	
 	move_input = Input.get_vector("Left", "Right", "Up", "Down")
 	
 	if Input.is_action_just_pressed("Down") or Input.is_action_just_pressed("Up") or Input.is_action_just_pressed("Left") or Input.is_action_just_pressed("Right"):
@@ -32,3 +41,18 @@ func _process(delta):
 	
 	
 	pan_sprite.global_position = move_input * offset_dist
+
+func button_tracker():
+	match desired_button:
+		av_buttons.LEFT:
+			label.text = "PRESS LEFT"
+		
+		av_buttons.RIGHT:
+			label.text = "PRESS RIGHT"
+		
+		av_buttons.UP:
+			label.text = "PRESS UP"
+		
+		av_buttons.DOWN:
+			label.text = "PRESS DOWN"
+	

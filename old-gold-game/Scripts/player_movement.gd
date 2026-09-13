a12e3e0c5e569308904f6c00d5fbda0ff6daeedf
featheredyogurt <aprilgeskus@outlook.com>
2026-09-13 14:37:58 +1000

    base movement done
    its not perfect buut its there
    - gotta relook at the tweening

---
+++ old-gold-game/Scripts/player_movement.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+@export var SPD = 10000
+@export var tileSize = 12
+@export var animSPD = 0.35
+var moving = false
+var input_dir
+
+func _physics_process(delta: float) -> void:
+	input_dir = Vector2.ZERO
+	if Input.is_action_pressed("ui_down"):
+		input_dir = Vector2(0,1)
+		move()
+	elif Input.is_action_pressed("ui_up"):
+		input_dir = Vector2(0,-1)
+		move()
+	elif Input.is_action_pressed("ui_right"):
+		input_dir = Vector2(1,0)
+		move()
+	elif Input.is_action_pressed("ui_left"):
+		input_dir = Vector2(-1,0)
+		move()
+	velocity = input_dir*SPD*delta
+	move_and_slide()
+
+func move():
+	if input_dir:
+		if moving == false:
+			moving == true
+			var tween = create_tween()
+			tween.tween_property(self, "position", position + input_dir*tileSize, animSPD)
+			tween.tween_callback(move_false)
+
+func move_false():
+	moving = false

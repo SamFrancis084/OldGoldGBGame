1b32a84b3c7ac4a7af5a6659ddce79f590837701
Sam Francis <samfrancis084@gmail.com>
2026-09-15 15:39:46 +1000

    fixes

--- old-gold-game/Scripts/player_movement.gd
+++ old-gold-game/Scripts/player_movement.gd
@@ -33,10 +33,11 @@ func move():
 	if input_dir:
 		if moving == false:
 			moving == true
-			var tween = create_tween()
-			tween.tween_property(self, "position", position + input_dir*tileSize, animSPD)
-			
-			tween.tween_callback(move_false)
+			#sam has annoyingly commented out this section for testing
+			#var tween = create_tween()
+			#tween.tween_property(self, "position", position + input_dir*tileSize, animSPD)
+			#
+			#tween.tween_callback(move_false)
 
 func move_false():
 	moving = false

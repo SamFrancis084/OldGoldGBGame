f643e3358ed332e3f4e7541e3cb1b77395bf2682
Sam Francis <samfrancis084@gmail.com>
2026-09-20 12:01:20 +1000

    implemented audio map

--- old-gold-game/Scripts/player_movement.gd
+++ old-gold-game/Scripts/player_movement.gd
@@ -8,9 +8,16 @@ var moving = false
 var input_dir
 
 @export_category("Audio") #footsteps
-@export var step_clip : AudioStream
+@export var default_step : AudioStream
+@export var rock_step : AudioStream
+@export var water_step : AudioStream
+
 @export var step_rate : float = 0.45
 var step_timer : float = 0.0
+
+@export var footstep_tile_map : TileMapLayer
+var rock_id : int = 0
+var water_id : int = 1
 
 func _physics_process(delta: float) -> void:
 	
@@ -31,16 +38,11 @@ func _physics_process(delta: float) -> void:
 	move(delta)
 	velocity = input_dir*SPD*delta
 	move_and_slide()
+	footstep_audio(delta)
 
 func move(delta):
-	step_timer -= delta
+	
 	if input_dir:
-		#audio
-		
-		if step_timer <= 0.0:
-			AudioTools.PlayClip(step_clip, -10, 1, true)
-			step_timer = step_rate
-		
 		if moving == false:
 			moving == true
 			#sam has annoyingly commented out this section for testing
@@ -48,6 +50,27 @@ func move(delta):
 			#tween.tween_property(self, "position", position + input_dir*tileSize, animSPD)
 			#
 			#tween.tween_callback(move_false)
+
+func footstep_audio(delta):
+	var step_clip : AudioStream = default_step
+	
+	if footstep_tile_map:
+		var local = footstep_tile_map.to_local(global_position)
+		var tile_pos = footstep_tile_map.local_to_map(local)
+		
+		var source_id = footstep_tile_map.get_cell_source_id(tile_pos)
+		match source_id:
+			rock_id:
+				step_clip = rock_step
+			water_id:
+				step_clip = water_step
+		print(source_id)
+	
+	step_timer -= delta
+	if input_dir:
+		if step_timer <= 0.0:
+			AudioTools.PlayClip(step_clip, 0, 1, true)
+			step_timer = step_rate
 
 func move_false():
 	moving = false
